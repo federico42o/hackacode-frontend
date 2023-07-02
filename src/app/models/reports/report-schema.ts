51a6b246826b6e0aa01ed6b11463e7a8ead90c5b
federico42o <fedes7777@gmail.com>
2023-07-01 22:13:54 -0300

    report component

---
+++ src/app/models/reports/report-schema.ts
@@ -0,0 +1,12 @@
+import { Buyer } from "../buyer";
+import { Employee } from "../employee";
+
+export interface ReportSchema {
+    totalTicketsSold?: number | null;
+    totalAmountSaleDay?: number | null;
+    totalAmountSaleMonthAndYear?: number | null;
+    totalAmountSaleYear?: number | null;
+    employees?: Employee[] | null ;
+    buyer?: Buyer | null ;
+    gameName?: string | null;
+}
--- src/app/modules/reports/reports.module.ts
+++ src/app/modules/reports/reports.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ReportsRoutingModule } from './reports-routing.module';
+import { ReportsComponent } from './pages/reports/reports.component';
+import { WidgetComponent } from './components/widget/widget.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ReportsComponent,
+    WidgetComponent
+  ],
   imports: [
     CommonModule,
     ReportsRoutingModule
+  ],
+  exports:[
+    ReportsComponent,
+    WidgetComponent
   ]
 })
 export class ReportsModule { }
