50a1ea5897f41cf21f77ee1776c223e552133d41
Federico Silva <50332167+federico42o@users.noreply.github.com>
2023-07-29 21:30:17 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production:true,
-    apiUrl:'http://vps-3471374-x.dattaweb.com:8080/api/',
-    tokenUrl:'http://vps-3471374-x.dattaweb.com:8080/token',
+    apiUrl:'https://vps-3471374-x.dattaweb.com:8080/api/',
+    tokenUrl:'https://vps-3471374-x.dattaweb.com:8080/token',
 };
